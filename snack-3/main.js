313257bab451b0e50bb50579e47f727901a39013
giacomomarongiu <giacomomarongiu1993@gmail.com>
2024-02-23 13:00:07 +0100

    little fix

--- snack-3/main.js
+++ snack-3/main.js
@@ -2,10 +2,8 @@
 let names = ['fabio', 'caterina',
     'filippa', 'simona'];
 
-//Dichiaro una variabile 
+//Dichiaro una variabile //Assegno a questa variabile un valore tramite input
 let user_name = prompt('insert name');
-
-//Assegno a questa variabile un valore tramite input
 
 console.log("Hai inserito i seguenti valori", user_name);
 
@@ -15,8 +13,8 @@ for (let index = 0; index < names.length; index++) {
 
     // SE è tra i miei array -> True
     // ->  Non mi interessa continuare e faccio finire l'iter
-    if (user_name == names[index]) {
-        console.log('benvenuto');
+    if (user_name === names[index]) {
+        result='benvenuto';
         index = names.length;
     }
 }
